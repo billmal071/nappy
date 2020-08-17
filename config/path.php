b4c19773add929375c8742e837c7909b4b88d379
nappystudio <nappystudiodev@gmail.com>
2020-08-17 08:33:56 +0000

    add config/path.php

---
+++ config/path.php
@@ -0,0 +1,26 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | File paths
+    |--------------------------------------------------------------------------
+    */
+
+    // Users Avatar and Cover Path
+    'avatar' => 'avatar/',
+    'cover' => 'cover/',
+    'img' => 'img/',
+    'img-category' => 'img-category/',
+    'cover' => 'cover/',
+
+    // Images Path
+    'uploads' => 'uploads/',
+    'files' => 'uploads/files/',
+    'large' => 'uploads/large/',
+    'medium' => 'uploads/medium/',
+    'small' => 'uploads/small/',
+    'preview' => 'uploads/preview/',
+    'thumbnail' => 'uploads/thumbnail/',
+);
